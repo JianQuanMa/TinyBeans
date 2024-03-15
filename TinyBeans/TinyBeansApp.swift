1122cec5295d0768e7c612ca281d223940439810
Jian Ma <jianquanma@gmail.com>
2024-03-15 13:38:59 -0400

    Initial Commit

---
+++ TinyBeans/TinyBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyBeansApp.swift
+//  TinyBeans
+//
+//  Created by Jian Ma on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct TinyBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
